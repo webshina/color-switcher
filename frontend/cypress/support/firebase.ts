af1656b447d8018f5e798b395b472dd1850e9b8a
KazukiYamashina <kazu4101719@gmail.com>
2023-07-13 13:23:11 +0900

    Fix

--- frontend/cypress/support/firebase.ts
+++ frontend/cypress/support/firebase.ts
@@ -1,12 +1,12 @@
-import { getApp, getApps, initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+// import { getApp, getApps, initializeApp } from 'firebase/app';
+// import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = Cypress.env('firebase');
+// const firebaseConfig = Cypress.env('firebase');
 
-if (getApps().length === 0) {
-  initializeApp(firebaseConfig);
-}
-const firebase = getApp();
+// if (getApps().length === 0) {
+//   initializeApp(firebaseConfig);
+// }
+// const firebase = getApp();
 
-export default firebase;
-export const firebaseAuth = getAuth(getApp());
+// export default firebase;
+// export const firebaseAuth = getAuth(getApp());
