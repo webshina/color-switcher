fe6026badd32d9c3c4eb9de9d5d7e18721179bc3
KazukiYamashina <kazu4101719@gmail.com>
2023-07-15 21:45:54 +0900

    Change log util Sentry -> winston

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,10 @@
-import * as Sentry from '@sentry/node';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import path from 'path';
+import winston from 'winston';
 import { localStoragePath } from './config/config';
-import { startDiscordBot } from './eventListeners/discord';
 import { router } from './routes';
 
 // env
@@ -16,24 +15,24 @@ dotenv.config({
   path: path.join(__dirname, `../.env.${process.env.NODE_ENV}.local`),
 });
 
-// Discord Bot
-startDiscordBot();
+// Set up Winston logger
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'discord-home' },
+  transports: [
+    new winston.transports.File({
+      filename: path.join(__dirname, '../logs/error.log'),
+      level: 'error',
+    }),
+    new winston.transports.File({
+      filename: path.join(__dirname, '../logs/combined.log'),
+    }),
+  ],
+});
 
 // Express
 const app = express();
-
-// Sentry
-Sentry.init({
-  dsn: 'https://14f4e779fdb14e2f961be5070c9cbb3b@o4505023359549440.ingest.sentry.io/4505519110619136',
-  integrations: [
-    new Sentry.Integrations.Http({ tracing: true }),
-    new Sentry.Integrations.Express({ app }),
-    ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
-  ],
-  tracesSampleRate: 1.0,
-});
-app.use(Sentry.Handlers.requestHandler());
-app.use(Sentry.Handlers.tracingHandler());
 
 // Middleware
 app.use(express.json());
@@ -41,6 +40,8 @@ const allowedOrigins = [
   'http://localhost:3004',
   'https://www.favo-community.com',
 ];
+
+// CORS
 app.use(
   cors({
     origin: function (origin, callback) {
@@ -63,17 +64,12 @@ app.use(cookieParser());
 // Static files
 app.use(express.static(localStoragePath));
 
+// router & controllers
 app.use('/', router);
 
-// Sentry error handler should be after all controllers
-app.use(Sentry.Handlers.errorHandler());
-
-// Handle uncaught exceptions
-process.on('uncaughtException', function (err) {
-  console.error('Caught exception:', err);
-  // Sentry
-  Sentry.captureException(err);
-  process.exit(1);
+// Handle uncaught exceptions and rejections
+process.on('uncaughtException', (err) => {
+  logger.error(err);
 });
 
 // Start the server
