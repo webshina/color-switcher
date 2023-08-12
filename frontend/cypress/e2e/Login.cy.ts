04e5dd700be086ece1f14b2569020a05a6d53900
KazukiYamashina <kazu4101719@gmail.com>
2023-08-12 16:23:07 +0900

    Init Cypress

---
+++ frontend/cypress/e2e/Login.cy.ts
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('Login Test', () => {
+  before(() => {});
+
+  beforeEach(() => {});
+
+  context('Validation', () => {
+    it('Can not login with wrong password', () => {});
+  });
+});
