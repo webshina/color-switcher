0c4a88eb5ef189068f40f8f47906af94aa5907bf
KazukiYamashina <kazu4101719@gmail.com>
2023-09-02 00:32:52 +0900

    Fix useManagementMembers

--- frontend/src/hooks/repository/useManagementMembers.ts
+++ frontend/src/hooks/repository/useManagementMembers.ts
@@ -3,14 +3,17 @@ import { get } from '@/utils/apiHelper';
 import useSWR from 'swr';
 
 export const useManagementMembers = (props: { guildId: number }) => {
-  const { ...swr } = useSWR('useManagementMembers', async () => {
-    try {
-      const res = await get(`/api/guild/${props.guildId}/management-members`);
-      return res.data as GuildMemberItem[];
-    } catch (e) {
-      return null;
+  const { ...swr } = useSWR(
+    ['useManagementMembers', props.guildId],
+    async () => {
+      try {
+        const res = await get(`/api/guild/${props.guildId}/management-members`);
+        return res.data as GuildMemberItem[];
+      } catch (e) {
+        return null;
+      }
     }
-  });
+  );
 
   return {
     ...swr,
