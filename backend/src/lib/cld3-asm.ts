20f1c4a94be5c1e2148b859a46f65546e97f7d00
KazukiYamashina <kazu4101719@gmail.com>
2023-10-07 13:23:09 +0900

    Fix loading of prismaClient & cld3Factory

--- backend/src/lib/cld3-asm.ts
+++ backend/src/lib/cld3-asm.ts
@@ -3,5 +3,10 @@ import { CldFactory, loadModule } from 'cld3-asm';
 const globalForCld3Factory = global as unknown as { cld3Factory: CldFactory };
 
 export const loadCld3Factory = async () => {
-  return globalForCld3Factory.cld3Factory || (await loadModule());
+  if (!globalForCld3Factory.cld3Factory) {
+    const cld3Factory = await loadModule();
+    globalForCld3Factory.cld3Factory = cld3Factory;
+  }
+
+  return globalForCld3Factory.cld3Factory;
 };
--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,9 +1,17 @@
+declare global {
+  var prisma: PrismaClient; // This must be a `var` and not a `let / const`
+}
+
 import { PrismaClient } from '@prisma/client';
+let prismaClient: PrismaClient;
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+if (process.env.NODE_ENV === 'production') {
+  prismaClient = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prismaClient = global.prisma;
+}
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['error', 'info', 'warn'],
-  });
+export const prisma = prismaClient;
